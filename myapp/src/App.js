764ab16da2e43c9df7309e0ed10135d1641175ac
moeinalipour <49764522+moeinalipour@users.noreply.github.com>
2023-06-13 13:12:57 +0330

    Add routing option in project

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import { CartProvider } from './Context/CartContext';
 import Navbar from './Navbar/navbar';
 import ProductsSection from './Products/ProductsSection';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ManagerPage from './pages/manager';
 
 function App() {
   return (
     <>
+      <Router>
         <CartProvider>
           <Navbar />
-          <ProductsSection />
+          <Routes>
+          <Route path="/" element={<ProductsSection />} />
+          <Route path="/manager" element={<ManagerPage />} />
+          </Routes>
         </CartProvider>
+      </Router>
     </>
   );
 }
 
 export default App;
-
-
---
+++ myapp/src/pages/manager.js
@@ -0,0 +1,5 @@
+export default function ManagerPage() {
+    return(
+        <h1>This page for add Product</h1>
+    );
+}
